369e98c4a55f96921ccd8c6cdca3a895b8d85087
seungje1217 <sjlim1217@naver.com>
2026-03-15 23:04:22 +0900

    서버 로직 수정

--- dist/pregnancy/pregnancy.service.d.ts
+++ dist/pregnancy/pregnancy.service.d.ts
@@ -4,7 +4,7 @@ import { PregnancyWeightLog } from '../entities/pregnancy-weight-log.entity';
 import { PregnancyCondition } from '../entities/pregnancy-condition.entity';
 import { CreatePregnancyDto } from './dto/create-pregnancy.dto';
 import { UpdatePregnancyDto } from './dto/update-pregnancy.dto';
-import { User } from 'src/user/user.entity';
+import { User } from '../user/user.entity';
 export declare class PregnancyService {
     private readonly pregnancyRepository;
     private readonly weightRepository;
@@ -26,6 +26,7 @@ export declare class PregnancyService {
         total_gain: number;
         due_date: Date;
         is_multiple: boolean | null;
+        bmi: number;
         max_allowed_bpm: number;
         conditions: import("../common/enums/condition.enum").ConditionType[];
     } | null>;
@@ -36,25 +37,62 @@ export declare class PregnancyService {
         title: any;
         guidelines: any;
     }>;
-    private calculateRecommendedWeight;
     private getCommonSymptoms;
     private getDefaultTip;
+    private calculateRecommendedWeight;
+    private calculateGuidelineWeight;
     getWeeklyHealth(userId: string): Promise<{
         week: number;
         recommended_weight_gain: string;
+        guideline_range_today: {
+            min: number;
+            max: number;
+        };
+        current_weight_status: string;
         common_symptoms: string[];
         today_tip: string;
     }>;
-    private calculateGuidelineWeight;
     calculateWeightTrend(userId: string): Promise<{
+        current_position: {
+            range: {
+                min: number;
+                max: number;
+            };
+            status: string;
+        };
         slope: number;
         status: string;
+        slope_status?: undefined;
         based_on?: undefined;
-        expected_slope?: undefined;
+        recommended_weekly_range?: undefined;
+    } | {
+        current_position: {
+            range: {
+                min: number;
+                max: number;
+            };
+            status: string;
+        };
+        slope: number;
+        slope_status: string;
+        status?: undefined;
+        based_on?: undefined;
+        recommended_weekly_range?: undefined;
     } | {
         based_on: string;
+        current_position: {
+            range: {
+                min: number;
+                max: number;
+            };
+            status: string;
+        };
         slope: number;
-        expected_slope: number;
-        status: string;
+        recommended_weekly_range: {
+            min: number;
+            max: number;
+        };
+        slope_status: string;
+        status?: undefined;
     }>;
 }
